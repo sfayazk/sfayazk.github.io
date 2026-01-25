8fe324fc6a1da5d9b5e027a4636a61fc8ba47351
sfayazk <thefayazk@gmail.com>
2026-01-25 23:04:44 +0530

    added new section

--- index.js
+++ index.js
@@ -27,3 +27,13 @@
     },
   }
 });
+
+function showSidebar(){
+  const sidebar = document.querySelector('.side-bar');
+  sidebar.style.display = 'flex';
+}
+
+function hideSidebar(){
+  const sidebar = document.querySelector('.side-bar');
+  sidebar.style.display = 'none';
+}
